fd565c87968c58c0dcb9f982ff19cebb69cc0b97
Lyon Van Voorhis <lyon.vanvoorhis@gmail.com>
2023-01-08 18:51:15 -0500

    more

--- ynab-reports/src/Components/TransactionListItem.tsx
+++ ynab-reports/src/Components/TransactionListItem.tsx
@@ -8,7 +8,26 @@ interface IProps {
 export const TransactionsListItem = observer(({ transaction }: IProps) => {
   return (
     <div>
-      <div>{transaction.id}</div>
+      <div>
+        <div>Id:</div>
+        <div>{transaction.id}</div>
+      </div>
+      <div>
+        <div>Account:</div>
+        <div>{transaction.account?.name}</div>
+      </div>
+      <div>
+        <div>Amount:</div>
+        <div>{transaction.amount}</div>
+      </div>
+      <div>
+        <div>Payee:</div>
+        <div>{transaction.payee?.name}</div>
+      </div>
+      <div>
+        <div>Category:</div>
+        <div>{transaction.category?.name}</div>
+      </div>
     </div>
   );
 });
