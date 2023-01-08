5d9d7f381fe2a7d88ee5acda23f14e23402a06b8
Lyon Van Voorhis <lyon.vanvoorhis@gmail.com>
2023-01-08 16:11:20 -0500

    remove transformers

--- ynab-reports/src/transformers/UserTransformer.ts
+++
@@ -1,6 +0,0 @@
-import { IUserAPIModel } from "../models/ApiModels/UserApiModel";
-import { IUser } from "../models/ClientModels/UserModel";
-
-export const userApiToClient = (userApi: IUserAPIModel): IUser => {
-  return { id: userApi.id };
-};
